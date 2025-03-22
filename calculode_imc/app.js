d9efde91b065c504457603922698b945a90fd619
Jardel Maciel <jardel.maciel22@gmail.com>
2025-03-21 22:30:32 -0300

    teste 1

--- calculode_imc/app.js
+++ calculode_imc/app.js
@@ -1,18 +1,9 @@
 function calcular(){
     var altura = document.getElementById('altura');
     var peso = document.getElementById('peso');
-    var pes = Number(peso.value)
-    var alt = Number(altura.value)
     var res = document.getElementById('res')
-    var imc = pes / (alt * alt);
-
-    res.innerHTML = imc.toFixed(1)
-
-    
-
-   
-
+    var alt = Number.altura
+    var pes = Number.peso
+    var imc = pes / (alt * alt)
+    res.innerHTML   
 }
-
-
-
