23014771dab429bf6958d2fc494285c3684db8d4
Jardel Maciel <jardel.maciel22@gmail.com>
2025-03-21 22:38:43 -0300

    Update app.js

--- calculode_imc/app.js
+++ calculode_imc/app.js
@@ -2,8 +2,8 @@ function calcular(){
     var altura = document.getElementById('altura');
     var peso = document.getElementById('peso');
     var res = document.getElementById('res')
-    var alt = Number.altura
-    var pes = Number.peso
+    var alt = Number(altura.value)
+    var pes = Number(peso.value)
     var imc = pes / (alt * alt)
-    res.innerHTML   
+    res.innerHTML = imc.toFixed(1)
 }
