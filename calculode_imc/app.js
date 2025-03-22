42e347ddf1f0ffd788529f86b9a0cf9120774bf8
Jardel Maciel <jardel.maciel22@gmail.com>
2025-03-22 10:34:56 -0300

    add condições

--- calculode_imc/app.js
+++ calculode_imc/app.js
@@ -6,4 +6,21 @@ function calcular(){
     var pes = Number(peso.value)
     var imc = pes / (alt * alt)
     res.innerHTML = imc.toFixed(1)
+    if(imc < 18.5 ){
+        res.innerHTML = ("Seu imc é " + imc.toFixed(1) + " Você está abaixo do peso.")
+    }if(imc >= 18.5 && imc <= 24.9){
+        res.innerHTML = ("Seu imc é " + imc.toFixed(1) + " Você está com peso normal .")
+
+    }if(imc >= 25 && imc <= 29.9){
+        res.innerHTML = ("Seu imc é " + imc.toFixed(1) + " Você está com sobrepeso.")
+    }if(imc >= 30 && imc <= 34.9){
+        res.innerHTML = ("Seu imc é " + imc.toFixed(1) + " Você está com Obesidade tipo 1.")
+    }if(imc >= 35 && imc <= 39.9){
+        res.innerHTML = ("Seu imc é " + imc.toFixed(1) + " Você está com Obesidade tipo 2.")
+    }if(imc >= 25 && imc <= 29.9){
+        res.innerHTML = ("Seu imc é " + imc.toFixed(1) + " Você está com sobrepeso.")
+    }if(imc >= 40 ){
+        res.innerHTML = ("Seu imc é " + imc.toFixed(1) + " Você está com Obesidade tipo 3.")
+    }
+    
 }
